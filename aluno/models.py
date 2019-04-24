bfc18f3366f2615032072f6046dd4344a6582624
jbs <joseliobeze@gmail.com>
2019-04-23 23:55:05 -0300

    incluido novos campo e ainda nao testados

--- aluno/models.py
+++ aluno/models.py
@@ -2,15 +2,41 @@ from django.db import models
 from datetime import date
 
 
-# Create your models here.
-class Aluno(models.Model):
+TIPO_SEXO = [
+    ('FEM', 'Feminino'),
+    ('MASC', 'Masculino')
+    ]
+
+
+class Endereços(models.Model):
+    pass
+
+class PaisResponsaveis(models.Model):
+    pass
+
+
+class Aluno(models.Model):  
     id_aluno = models.AutoField('Id do Aluno', primary_key=True)
     num_matricula = models.CharField('Matricula', max_length=50, blank=True)
     nome = models.CharField('Nome', max_length=30, blank=False)
     sobrenome = models.CharField('Sobre Nome', max_length=100, blank=False)
     nascimento = models.DateField('Nascimento', auto_now_add=False)    
+    rg = models.CharField('RG/Certidão de nascimento', max_length=50, blank=True)
+    cpf = models.CharField('CPF', max_length=14, blank=True)
+    sexo = models.CharField('Sexo', max_length=4, choices=TIPO_SEXO, blank=True)
+    altuna = models.DecimalField('Altura', max_digits=8, decimal_places=2, default=0, blank=True)
+    peso = models.DecimalField('Peso', max_digits=8, decimal_places=4, default=0, blank=True)
+    tipo_sanguinio = models.CharField('Tipo Sanguinio', max_length=11, blank=True)
+    endereco = models.ForeignKey(Endereços, on_delete=models.CASCADE, verbose_name='Endereço', blank=True)
+
     data_cadastro = models.DateTimeField('Cadastrado em', auto_now_add=True)
     data_atualização = models.DateTimeField('Atualizado em', auto_now=True)
+
+    @property
+    def contAlunos(self):
+        """ Conta a quantidade de aluno: Obs.. ainda não foi testada """
+        cadastros=Aluno.objects.all()
+        return cadastros.count()
 
 
     @property
--- aluno/tests.py
+++ aluno/tests.py
@@ -11,12 +11,24 @@ class AlunoTestCase(TestCase):
         self.aluno.sobrenome = 'Silva'
         self.aluno.nascimento = date(2000,9,28)
 
+        self.aluno1=Aluno()
+        self.aluno1.nome = 'marcelo'
+        self.aluno1.sobrenome = 'borges'
+        self.aluno1.nascimento = date(2001,10,28)
+      
+
 
     def testnome_completo(self):
         self.assertEquals(self.aluno.nome_completo,'Joselio Silva')
+        self.assertEquals(self.aluno1.nome_completo, 'marcelo borges')
 
     def testnascimento(self):
-        self.assertEquals(self.aluno.nascimento, date(2000,9,28))
+        self.assertEquals(self.aluno.nascimento,  date(2000,9,28))
+        self.assertEquals(self.aluno1.nascimento,  date(2001,10,28))
+
 
     def testidade(self):
         self.assertEquals(self.aluno.idade, 18)
+        self.assertEquals(self.aluno1.idade, 17)
+
+        
