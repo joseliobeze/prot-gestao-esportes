a63800348d69fed5304ecf978cfe605129d9ffa2
jbs <joseliobeze@gmail.com>
2019-04-24 23:50:42 -0300

    realizaçao de novos testes

--- aluno/models.py
+++ aluno/models.py
@@ -14,30 +14,31 @@ class Endereços(models.Model):
 class PaisResponsaveis(models.Model):
     pass
 
-
 class Aluno(models.Model):  
     id_aluno = models.AutoField('Id do Aluno', primary_key=True)
     num_matricula = models.CharField('Matricula', max_length=50, blank=True)
     nome = models.CharField('Nome', max_length=30, blank=False)
     sobrenome = models.CharField('Sobre Nome', max_length=100, blank=False)
     nascimento = models.DateField('Nascimento', auto_now_add=False)    
-    rg = models.CharField('RG/Certidão de nascimento', max_length=50, blank=True, null=True)
-    cpf = models.CharField('CPF', max_length=14, blank=True, null=True)
+    rg = models.IntegerField('RG/Certidão de nascimento', blank=True, null=True)
+    cpf = models.IntegerField('CPF', blank=True, null=True)
     sexo = models.CharField('Sexo', max_length=4, choices=TIPO_SEXO, blank=True, null=True)
-    altuna = models.DecimalField('Altura', max_digits=8, decimal_places=2, default=0, blank=True, null=True)
+    altura = models.DecimalField('Altura', max_digits=8, decimal_places=2, default=0, blank=True, null=True)
     peso = models.DecimalField('Peso', max_digits=8, decimal_places=4, default=0, blank=True, null=True)
     tipo_sanguinio = models.CharField('Tipo Sanguinio', max_length=11, blank=True, null=True)
-    endereco = models.ForeignKey(Endereços, on_delete=models.CASCADE, verbose_name='Endereço', blank=True, null=True)
-
+    cep = models.IntegerField('Cep', blank=True, null=True)
+    estado = models.CharField('Estado', max_length=30, blank=True, null=True, default='')
+    cidade = models.CharField('Estado', max_length=50, blank=True, null=True, default='')
+    rua = models.CharField('Estado', max_length=50, blank=True, null=True, default='')
     data_cadastro = models.DateTimeField('Cadastrado em', auto_now_add=True)
-    data_atualização = models.DateTimeField('Atualizado em', auto_now=True)
+    data_atualizacão = models.DateTimeField('Atualizado em', auto_now=True)
+
 
     @property
     def contAlunos(self):
         """ Conta a quantidade de aluno: Obs.. ainda não foi testada """
         cadastros=Aluno.objects.all()
         return cadastros.count()
-
 
     @property
     def idade(self):
