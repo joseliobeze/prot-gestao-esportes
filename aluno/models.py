255dd91edc375e336e8650a4f9df641b2a04f72c
jbs <joseliobeze@gmail.com>
2019-04-28 11:18:46 -0300

    mudanças notest

--- aluno/models.py
+++ aluno/models.py
@@ -23,8 +23,8 @@ class dadosEscolinha(models.Model):
 class Aluno(models.Model):  
     id_aluno = models.AutoField('Id do Aluno', primary_key=True)
     num_matricula = models.CharField('Matricula', max_length=50, blank=True)
-    nome = models.CharField('Nome', max_length=30, blank=False)
-    sobrenome = models.CharField('Sobre Nome', max_length=100, blank=False)
+    nome = models.CharField('Nome', max_length=30, blank=False, null=False)
+    sobrenome = models.CharField('Sobre Nome', max_length=100, blank=False, null=False)
     nascimento = models.DateField('Nascimento', auto_now_add=False)    
     rg = models.CharField('RG/Certidão de nascimento', max_length=30, blank=True, null=True)
     cpf = models.CharField('CPF', max_length=14,blank=True, null=True)
@@ -48,28 +48,34 @@ class Aluno(models.Model):
     data_cadastro = models.DateTimeField('Cadastrado em', auto_now_add=True)
     data_atualizacão = models.DateTimeField('Atualizado em', auto_now=True)
 
-
+    
     @property
     def contAlunos(self):
         """ Conta a quantidade de aluno """
         cadastros = Aluno.objects.all()
         return cadastros.count()
 
+
     @property
     def idade(self):
+        " Retorna a idade."
         hoje = date.today()
         idade = hoje.year - self.nascimento.year - ((hoje.month, hoje.day) < (self.nascimento.month, self.nascimento.day))
         return idade
+    
      
     @property
     def nome_completo(self):
         "Retorna o nome completo do aluno"
         return f'{self.nome} {self.sobrenome}'
 
-    class Meta:
-        ordering = ['nome']
-        verbose_name = 'Alunos'
-        verbose_name_plural = 'Alunos'
-    
+
     def __str__(self):
         return self.nome_completo
+
+
+    class Meta:
+        ordering = ['nome']
+        
+    
+    
--- aluno/tests.py
+++ aluno/tests.py
@@ -3,41 +3,26 @@ from .models import Aluno
 from datetime import date
 
 
-class AlunoTestCase(TestCase):
-    def testcampos_models(self):
-        """testes de campo e e comportamento das açoes e funções"""
-        aluno = Aluno()
-        aluno.num_matricula = 'turma12'
-        aluno.nome = 'Joselio'
-        aluno.sobrenome = 'Silva'
-        aluno.nascimento = date(2000,9,28)
-        aluno.rg = 123456789
-        aluno.cpf = 987654321
-        aluno.sexo = 'Masculino'
-        aluno.altura = 1.65
-        aluno.peso = 45.50
-        aluno.tipo_sanguinio = 'AB'
-        aluno.email = 'teste@gmail.com'
-        
-        aluno.cep = 65750000
-        aluno.estado = 'GO'
-        aluno.cidade = 'Aguas Lindas'
-        aluno.rua = 'sem nome'
-        aluno.data_cadastro = date.today()
-        aluno.data_atualizacão = date.today()
+class AlunoModelsTest(TestCase):
+    def test_metodo_contaAlunos(self):
+        aluno=Aluno(nascimento=date(2000,1,1))
         aluno.save()
-      
-
-        self.assertIs(aluno.id_aluno, 1)
-        self.assertIs(aluno.num_matricula, 'turma12')
-        self.assertEquals(aluno.nome_completo,'Joselio Silva')
-        self.assertEquals(aluno.nascimento, date(2000,9,28))
-        self.assertIs(aluno.rg, 123456789)
-        self.assertEqual(aluno.sexo, 'Masculino')
-        self.assertEquals(aluno.idade, 18)
-        self.assertIs(aluno.email, aluno.nome)
-        self.assertIs(aluno.contAlunos, 1)
+        self.assertEqual(1, int(aluno.contAlunos))        
         aluno.delete()
-        self.assertIs(aluno.contAlunos, 0)
+        self.assertEqual(0, int(aluno.contAlunos))
 
+
+    def test_metodo_idade(self):
+        aluno=Aluno(nascimento=date(2000,1,1))
+        self.assertEqual(19, int(aluno.idade))
         
+ 
+    def test_mostra_string(self):
+        aluno=Aluno(nome='Joselio', sobrenome='Silva')
+        self.assertEqual(str(aluno), f'{aluno.nome} {aluno.sobrenome}')
+
+
+    def test_class_meta(self):
+        self.assertEqual(['nome'], Aluno._meta.ordering)
+        self.assertEqual('aluno', str(Aluno._meta.verbose_name))
+        self.assertEqual('alunos', str(Aluno._meta.verbose_name_plural))
